8c1f256bba97175d03ca988ea857077ce17cf903
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-10-20 18:42:53 +0800

    structure conventions fixed

--- reddit_main.py
+++ reddit_main.py
@@ -8,110 +8,118 @@ from praw.models import MoreComments # used to ignore 'AttributeError: 'MoreComm
 # Submissions docs: https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
 # Comments docs: https://praw.readthedocs.io/en/latest/code_overview/models/comment.html
 
-# Functions 
+CLIENT_ID = 'YOUR PERSONAL USE SCRIPT (14 CHARS)'
+CLIENT_SECRET = 'YOUR SECRET KEY (27 CHARS)'
+USER_AGENT = 'YOUR APP NAME'
+USERNAME = 'YOUR REDDIT USERNAME'
+PASSWORD = 'YOUR REDDIT PASSWORD'
 
 def get_date(created):
+    """
+    Converts into correct datetime format
+    """
     return dt.datetime.fromtimestamp(created)
 
 # Instantiation
+def main():
+    """
+    Initiation docstring
+    """
 
-client_id = 'YOUR PERSONAL USE SCRIPT (14 CHARS)'
-client_secret = 'YOUR SECRET KEY (27 CHARS)'
-user_agent = 'YOUR APP NAME'
-username = 'YOUR REDDIT USERNAME'
-password = 'YOUR REDDIT PASSWORD'
+    credentials = {}
+    credentials['CLIENT_ID'] = CLIENT_ID
+    credentials['CLIENT_SECRET'] = CLIENT_SECRET
+    credentials['USER_AGENT'] = USER_AGENT
+    credentials['USERNAME'] = USERNAME
+    credentials['PASSWORD'] = PASSWORD
 
-credentials = {}
-credentials['CLIENT_ID'] = client_id
-credentials['CLIENT_SECRET'] = client_secret
-credentials['USER_AGENT'] = user_agent
-credentials['USERNAME'] = username
-credentials['PASSWORD'] = password
+    # Use this first time (with your credentials in the start) to initialize your json file
+    #with open("reddit_credentials.json", "w") as file:
+    #    json.dump(credentials, file)
 
-# Use this first time (with your credentials in the start) to initialize your json file
-#with open("reddit_credentials.json", "w") as file:
-#    json.dump(credentials, file)
+    # Use this after initializing your credentials
+    with open("reddit_credentials.json", "r") as file:
+        creds = json.load(file)
 
-# Use this after initializing your credentials
-with open("reddit_credentials.json", "r") as file:
-    creds = json.load(file)
+    # Initiate with credentials (you may have to comment this out when first initializing your json file)
+    reddit = praw.Reddit(client_id=creds["CLIENT_ID"], \
+                        client_secret=creds["CLIENT_SECRET"], \
+                        user_agent=creds["USER_AGENT"], \
+                        username=creds["USERNAME"], \
+                        password=creds["PASSWORD"])
 
-# Initiate with credentials (you may have to comment this out when first initializing your json file)
-reddit = praw.Reddit(client_id=creds["CLIENT_ID"], \
-                    client_secret=creds["CLIENT_SECRET"], \
-                    user_agent=creds["USER_AGENT"], \
-                    username=creds["USERNAME"], \
-                    password=creds["PASSWORD"])
+    # TODO: Add several of subreddits
+    # subreddits = reddit.subreddit('singapore+suicidewatch+askreddit') ie, hardcoding our subs.
+    subreddit = reddit.subreddit('singapore')
 
-# TODO: Add several of subreddits
-# subreddits = reddit.subreddit('singapore+suicidewatch+askreddit') ie, hardcoding our subs.
-subreddit = reddit.subreddit('singapore')
+    # Each subreddit has five different ways of organizing the topics created by redditors:
+    #   .hot, .new, .controversial, .top, .gilded
+    # Can also use subreddit.search("KEYWORD") to get matching results
+    top_subreddits = subreddit.top(limit=10) # Getting top 100 up-voted topics of all time in r/singapore
 
-# Each subreddit has five different ways of organizing the topics created by redditors:
-#   .hot, .new, .controversial, .top, .gilded
-# Can also use subreddit.search("KEYWORD") to get matching results
-top_subreddits = subreddit.top(limit=10) # Getting top 100 up-voted topics of all time in r/singapore
+    topics_dict = { "author": [],
+                "title":[],
+                "score":[],
+                "id":[], "url":[],
+                "comms_num": [],
+                "created": [],
+                "body":[]}
 
-topics_dict = { "author": [],
-            "title":[],
-            "score":[],
-            "id":[], "url":[],
-            "comms_num": [],
-            "created": [],
-            "body":[]}
+    # Checking information of top 10 posts in r/singapore
+    for submission in top_subreddits:
+        topics_dict["author"].append(submission.author)
+        topics_dict["title"].append(submission.title)
+        topics_dict["score"].append(submission.score)
+        topics_dict["id"].append(submission.id)
+        topics_dict["url"].append(submission.url)
+        topics_dict["comms_num"].append(submission.num_comments)
+        topics_dict["created"].append(submission.created)
+        topics_dict["body"].append(submission.selftext)
 
-# Checking information of top 10 posts in r/singapore
-for submission in top_subreddits:
-    topics_dict["author"].append(submission.author)
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
+    # Convert dictionary to dataframe to make data more easily readable
+    topics_data = pd.DataFrame(topics_dict)
 
-# Convert dictionary to dataframe to make data more easily readable
-topics_data = pd.DataFrame(topics_dict)
+    # Fixing column date. We define it, call it, and join the new column to the dataset.
+    _timestamp = topics_data["created"].apply(get_date)
+    topics_data = topics_data.assign(timestamp = _timestamp)
 
-# Fixing column date. We define it, call it, and join the new column to the dataset.
-_timestamp = topics_data["created"].apply(get_date)
-topics_data = topics_data.assign(timestamp = _timestamp)
+    # TODO: Close each time you run if not doing tests
+    # Opens (or creates) the csv file and writes data to each column. Easily done with pandas .to_csv function.
+    with open('reddit_table.csv', 'w+', encoding="utf-8") as file:
+        topics_data.to_csv(file, index=False) 
 
-# TODO: Close each time you run if not doing tests
-# Opens (or creates) the csv file and writes data to each column. Easily done with pandas .to_csv function.
-with open('reddit_table.csv', 'w+', encoding="utf-8") as file:
-    topics_data.to_csv(file, index=False) 
+    ########################################################################################################
+    # Getting particular submission
+    # First, we need to obtain a submission object. There are 2 ways to do this.
+    # 1) Retrieve by URL
+    # 2) Retrieve by submission ID (which we happen to know, it is '50ycve')
 
-########################################################################################################
-# Getting particular submission
-# First, we need to obtain a submission object. There are 2 ways to do this.
-# 1) Retrieve by URL
-# 2) Retrieve by submission ID (which we happen to know, it is '50ycve')
+    submission = reddit.submission(url='https://www.reddit.com/r/singapore/comments/cfiwzc/feeling_isolated_stressed_and_depressed_more/')
+    #submission = reddit.sumission(id='50ycve')
 
-submission = reddit.submission(url='https://www.reddit.com/r/singapore/comments/cfiwzc/feeling_isolated_stressed_and_depressed_more/')
-#submission = reddit.sumission(id='50ycve')
+    # MoreComments objects will be replaced until there are none left, as long as they satisfy the threshold.
+    submission.comments.replace_more(limit=None)
 
-# MoreComments objects will be replaced until there are none left, as long as they satisfy the threshold.
-submission.comments.replace_more(limit=None)
+    # OPTION 1 (Not that robust solution)
+    # Handling comments and replies
+    for comment in submission.comments:
+        if isinstance(comment, MoreComments):
+            continue
 
-# OPTION 1 (Not that robust solution)
-# Handling comments and replies
-for comment in submission.comments:
-    if isinstance(comment, MoreComments):
-        continue
+        # Printing comments (top level comments)
+        print("Comment: " + str(comment.body))
 
-    # Printing comments (top level comments)
-    print("Comment: " + str(comment.body))
+        # Printing reples (second level comments)
+        for reply in comment.replies:
+            print("Reply: " + str(reply.body))
 
-    # Printing reples (second level comments)
-    for reply in comment.replies:
-        print("Reply: " + str(reply.body))
+    # OPTION 2 (A more robust solution)
+    # Handling comments and replies (even if it's arbitrarily deep)
+    # Breadth-first traversal, going through all top level comments, then all secondary comments, and so on.
+    comment_queue = submission.comments[:]
+    for comment in submission.comments.list():
+        print("=== Author: ", comment.author, "===")
+        print(comment.body)
 
-# OPTION 2 (A more robust solution)
-# Handling comments and replies (even if it's arbitrarily deep)
-# Breadth-first traversal, going through all top level comments, then all secondary comments, and so on.
-comment_queue = submission.comments[:]
-for comment in submission.comments.list():
-    print("=== Author: ", comment.author, "===")
-    print(comment.body)
+if __name__ == "__main__": 
+    main()
--- twitter_main.py
+++ twitter_main.py
@@ -10,107 +10,116 @@ from twython import Twython
 from twython import TwythonStreamer
 from collections import Counter
 
-consumer_key = 'PUT KEY HERE'
-consumer_secret = 'PUT SECRET HERE'
-access_token = 'PUT TOKEN HERE'
-access_secret = 'PUT SECRET HERE'
+CONSUMER_KEY = 'PUT KEY HERE'
+CONSUMER_SECRET = 'PUT SECRET HERE'
+ACCESS_TOKEN = 'PUT TOKEN HERE'
+ACCESS_SECRET = 'PUT SECRET HERE'
 
-credentials = {}
-credentials['CONSUMER_KEY'] = consumer_key
-credentials['CONSUMER_SECRET'] = consumer_secret
-credentials['ACCESS_TOKEN'] = access_token
-credentials['ACCESS_SECRET'] = access_secret
+def main():
+    """
+    Initiation #TODO
+    """
+    credentials = {}
+    credentials['CONSUMER_KEY'] = CONSUMER_KEY
+    credentials['CONSUMER_SECRET'] = CONSUMER_SECRET
+    credentials['ACCESS_TOKEN'] = ACCESS_TOKEN
+    credentials['ACCESS_SECRET'] = ACCESS_SECRET
 
-# (1) If you haven't saved your json file, use this template first!
-#with open("twitter_credentials.json", "w") as file:
-#    json.dump(credentials, file)
+    # (1) If you haven't saved your json file, use this template first!
+    #with open("twitter_credentials.json", "w") as file:
+    #    json.dump(credentials, file)
 
-# (2) After you saved your json file using your above stated credentials, use this template.
-with open("twitter_credentials.json", "r") as file:
-    creds = json.load(file)
+    # (2) After you saved your json file using your above stated credentials, use this template.
+    with open("twitter_credentials.json", "r") as file:
+        creds = json.load(file)
 
-python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+    python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
-# Create our query
-query = {'q': 'HongKong',
-        'result_type': 'popular',
-        'count': 10,
-        'lang': 'en',
-        }
+    # Create our query
+    query = {'q': 'HongKong',
+            'result_type': 'popular',
+            'count': 10,
+            'lang': 'en',
+            }
 
-# Search tweets
-dict_ = {
-        'user': [],
-        'date': [],
-        'text': [],
-        'favorite_count': []
-        }
+    # Search tweets
+    dict_ = {
+            'user': [],
+            'date': [],
+            'text': [],
+            'favorite_count': []
+            }
 
-for status in python_tweets.search(**query)['statuses']:
-    dict_['user'].append(status['user']['screen_name'])
-    dict_['date'].append(status['created_at'])
-    dict_['text'].append(status['text'])
-    dict_['favorite_count'].append(status['favorite_count'])
+    for status in python_tweets.search(**query)['statuses']:
+        dict_['user'].append(status['user']['screen_name'])
+        dict_['date'].append(status['created_at'])
+        dict_['text'].append(status['text'])
+        dict_['favorite_count'].append(status['favorite_count'])
 
-# Structure data in a pandas DataFrame for easier manipulation
-df = pd.DataFrame(dict_)
-df.sort_values(by='favorite_count', inplace=True, ascending=False)
-df.head(5)
+    # Structure data in a pandas DataFrame for easier manipulation
+    df = pd.DataFrame(dict_)
+    df.sort_values(by='favorite_count', inplace=True, ascending=False)
+    df.head(5)
 
-# Filter out unwanted data
-def process_tweet(tweet):
+    # Filter out unwanted data
+    def process_tweet(tweet):
 
-    # TODO: Add wanted data
-    d = {}
-    d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
-    d['text'] = tweet['text'].encode('utf-8')
-    d['user'] = tweet['user']['screen_name'].encode('utf-8')
-    
-    if tweet['user']['location'] != None:
-        d['user_loc'] = tweet['user']['location'].encode('utf-8')
-    else:
-        d['user_loc'] = ''
-    
-    return d
-    
-def find_encoding(fname):
-    r_file = open(fname, 'rb').read()
-    result = chardet.detect(r_file)
-    charenc = result['encoding']
-    return charenc
-
-# Create a class that inherits TwythonStreamer
-class MyStreamer(TwythonStreamer):     
-
-    # Received data
-    def on_success(self, data):
-
-        # Only collect tweets in English
-        if data['lang'] == 'en':
-            tweet_data = process_tweet(data)
-            self.save_to_csv(tweet_data)
-
-    # Problem with the API
-    def on_error(self, status_code, data):
-        print(status_code, data)
-        self.disconnect()
+        # TODO: Add wanted data
+        d = {}
+        d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+        d['text'] = tweet['text'].encode('utf-8')
+        d['user'] = tweet['user']['screen_name'].encode('utf-8')
         
-    # Save each tweet to csv file
-    def save_to_csv(self, tweet):
-        with open(r'twitter_table.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(list(tweet.values()))
+        if tweet['user']['location'] != None:
+            d['user_loc'] = tweet['user']['location'].encode('utf-8')
+        else:
+            d['user_loc'] = ''
+        
+        return d
+        
+    def find_encoding(fname):
+        r_file = open(fname, 'rb').read()
+        result = chardet.detect(r_file)
+        charenc = result['encoding']
+        return charenc
 
-# Instantiate from our streaming class
-stream = MyStreamer(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], 
-                    creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+    # Create a class that inherits TwythonStreamer
+    class MyStreamer(TwythonStreamer):
+        """
+        Initiates TwythonStreamer object
+        """
+        # Received data
+        def on_success(self, data):
 
-# Start the stream
-# TODO: Add our keywords
-query_terms = ['stress', 'depressed', 'tired', 'sad', 'hate']
-stream.statuses.filter(track=query_terms)
+            # Only collect tweets in English
+            if data['lang'] == 'en':
+                tweet_data = process_tweet(data)
+                self.save_to_csv(tweet_data)
 
-my_encoding = find_encoding('twitter_table.csv')
-tweets = pd.read_csv('twitter_table.csv', encoding=my_encoding, names=['hashtags', 'text', 'user', 'location'])
+        # Problem with the API
+        def on_error(self, status_code, data):
+            print(status_code, data)
+            self.disconnect()
+            
+        # Save each tweet to csv file
+        def save_to_csv(self, tweet):
+            with open(r'twitter_table.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(list(tweet.values()))
 
-tweets.head()
+    # Instantiate from our streaming class
+    stream = MyStreamer(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], 
+                        creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+
+    # Start the stream
+    # TODO: Add our keywords
+    query_terms = ['stress', 'depressed', 'tired', 'sad', 'hate']
+    stream.statuses.filter(track=query_terms)
+
+    my_encoding = find_encoding('twitter_table.csv')
+    tweets = pd.read_csv('twitter_table.csv', encoding=my_encoding, names=['hashtags', 'text', 'user', 'location'])
+
+    tweets.head()
+
+if __name__ == "__main__": 
+    main()
